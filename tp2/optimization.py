1fd6a534111e4303f98ec9109ce8de98078cbc4a
João Pires Barreira <jpiresbarreira@gmail.com>
2018-12-02 15:38:19 +0000

    Hiper-parameterização parte 3

--- tp2/optimization.py
+++ tp2/optimization.py
@@ -1,31 +1,35 @@
 import pandas as pd
-from sklearn import preprocessing
-from sklearn.feature_selection import SelectKBest, f_classif
-from sklearn.model_selection import RandomizedSearchCV
 import scipy.stats as ss
 import numpy as np
 
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.model_selection import RandomizedSearchCV
 
+
+__author__ = "André Pereira, Carlos Lemos, João Barreira, Rafael Braga"
+__email__ = "pg38923@alunos.uminho.pt, pg38410@alunos.uminho.pt, a73831@alunos.uminho.pt, a61799@alunos.uminho.pt"
+
+
+# Imprime no terminal top-X (por defeito = 3) de resultados dos modelos testados
 def report(results, n_top=3):
     for i in range(1, n_top + 1):
         candidates = np.flatnonzero(results['rank_test_score'] == i)
         for candidate in candidates:
             print("Model with rank: {0}".format(i))
-            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+            print("Mean validation score: {0:.3f} (+/- {1:.3f})".format(
                   results['mean_test_score'][candidate],
                   results['std_test_score'][candidate]))
-            print("Parameters: {0}".format(results['params'][candidate]))
-            print("")
+            print("Parameters: {0}\n".format(results['params'][candidate]))
 
 
 # Ler dados do ficheiro .csv
-from sklearn.svm import SVC
 
 data = pd.read_csv("../datasets/Dataset_MousePSS.csv", sep=';')
 
 # Remover ExamID e StudyID
 
-data = data.drop('ExamID', 1)
+# data = data.drop('ExamID', 1)
 data = data.drop('StudyID', 1)
 
 # Retirar coluna de output 'PSS_Stress' do conjunto de dados (para variável auxiliar)
@@ -35,40 +39,63 @@ data = data.drop('PSS_Stress', 1)
 
 # Substituir NaN por valor da mediana
 
-data = data.fillna(data.median())
+for key in data.keys():
+    data[key] = data[key].fillna(data[key].median())
+
+# Feature selection
+
+cols = [9, 12]
+cols_names = list(data.columns[cols])
+data = data[cols_names]
+
+# selector = SelectKBest(f_regression, k=3)
+# selector.fit(data, target)
+# cols = selector.get_support(indices=True)
+# cols_names = list(data.columns[cols])
+
+for idx, (ci, cn) in enumerate(zip(cols, cols_names)):
+    print("*" * (len(cols) - idx) + " " * idx, ci, cn)
+
+# print(data.keys())
+
+# Standardizar
+
+# scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+# values_standardized = scaler.fit_transform(data.values)
+# data = pd.DataFrame(values_standardized, columns=data.columns)
 
 robust_scaler = preprocessing.RobustScaler()
 values_standardized = robust_scaler.fit_transform(data.values)
 data = pd.DataFrame(values_standardized, columns=data.columns)
 
-# Feature selection
-
-selector = SelectKBest(f_classif, k=5)
-selector.fit(data, target)
-cols = selector.get_support(indices=True)
-cols_names = list(data.columns[cols])
-
-for idx, (ci, cn) in enumerate(zip(cols, cols_names)):
-    print("*" * (len(cols) - idx) + " " * idx, ci, cn)
-
-data = data[cols_names]
-
 # Hyper-parameterização
 
 clf_model = SVC()
 
-param_dist = {'C': ss.randint(1,10),
+param_dist = {'C': np.random.uniform(low=0.0, high=2.0, size=(10,)),
               'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
-              'degree': ss.randint(1, 20),
+              'degree': ss.randint(1, 5),
               'gamma': ['auto', 'scale'],
-              # 'coef0': np.random.uniform(low=0.0, high=20.0, size=(10,)),
-              'coef0': ss.randint(1,10),
+              'coef0': np.random.uniform(low=0.0, high=2.0, size=(10,)),
               'shrinking': [True, False],
               'probability': [True, False],
-              'tol': ss.uniform,
+              'tol': np.random.uniform(low=0.0, high=2.0, size=(10,)),
               'decision_function_shape' : ['ovo', 'ovr']}
 
-n_iter = 20
-rs = RandomizedSearchCV(clf_model, param_distributions=param_dist, n_iter=n_iter, cv=5)
+# clf_model = GaussianProcessClassifier()
+#
+# param_dist = {
+#               # 'n_restarts_optimizer': ss.randint(1, 10),
+#               # 'max_iter_predict': ss.randint(1, 10),
+#               # 'warm_start': [True, False],
+#               # 'copy_X_train': [True, False],
+#               # 'random_state': ss.randint(1, 10),
+#               'multi_class': ['one_vs_rest', 'one_vs_one'],
+#               # 'n_jobs': [-1]
+#               }
+
+# Cálculo dos resultados (com hiper-parameterização a 50 iterações e 5-fold cross validation)
+
+rs = RandomizedSearchCV(clf_model, param_distributions=param_dist, n_iter=50, cv=5)
 rs.fit(data, target)
 report(rs.cv_results_)
