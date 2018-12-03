4fcb029d73840cd8d0f05c149d8177c4d49cc8c2
João Pires Barreira <jpiresbarreira@gmail.com>
2018-12-03 13:04:53 +0000

    Hiper-parametrização, Feature selection manual e binarização do output

--- tp2/ml.py
+++ tp2/ml.py
@@ -1,22 +1,30 @@
-import pandas as pd
-import os
 from sklearn import preprocessing
-# from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.model_selection import cross_val_score, ShuffleSplit
-from matplotlib import pyplot as plt
-from math import sqrt
-
+from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+from math import sqrt
+
+
+__author__ = "André Pereira, Carlos Lemos, João Barreira, Rafael Braga"
+__email__ = "pg38923@alunos.uminho.pt, pg38410@alunos.uminho.pt, a73831@alunos.uminho.pt, a61799@alunos.uminho.pt"
+
 
 def mkdir(dir_name):
+    '''
+    Cria pasta cujo path é recebido como parâmetro.
+
+    :param dir_name: Path para a pasta a ser criada
+    '''
     try:
         os.rmdir(dir_name)
     except OSError:
@@ -27,14 +35,96 @@ def mkdir(dir_name):
         pass
 
 
+def normalizacao_e_resultados(data):
+    print("### Normalization ###")
+
+    # Normalizar dados
+
+    normalizer = preprocessing.Normalizer(norm='l1') # 'l2' (mesmos resultados)
+    values_normalized = normalizer.transform(data.values)
+    data = pd.DataFrame(values_normalized, columns=data.columns)
+
+    # Criar modelos
+
+    clf_models = [
+        KNeighborsClassifier(),
+        SVC(),
+        GaussianProcessClassifier(),
+        DecisionTreeClassifier(),
+        RandomForestClassifier(),
+        MLPClassifier(),
+        AdaBoostClassifier(),
+        GaussianNB()
+    ]
+
+    clf_names = ["Nearest Neighbors", "SVM", "Gaussian Process", "Decision Tree", "Random Forest", "Neural Net",
+                 "AdaBoost", "Naive Bayes", "QDA"]
+
+    # K-Fold Cross-validation e Calcular resultados
+
+    for name, clf in zip(clf_names, clf_models):
+        # cv = ShuffleSplit(n_splits=10, test_size=0.6, random_state=0)
+        scores = cross_val_score(clf, data, target, cv=5)
+        print(name, "Accuracy: %0.6f (+/- %0.6f)" % (scores.mean(), scores.std() * 2))
+
+
+def standardizacao_e_resultados(data):
+    print("\n### Standardization ###")
+
+    # Standardizar dados
+
+    # scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+    # values_standardized = scaler.fit_transform(data.values)
+    # data = pd.DataFrame(values_standardized, columns=data.columns)
+
+    # Quando temos outliers, é melhor usar RobustScaler (in doc sklearn)
+    robust_scaler = preprocessing.RobustScaler()
+    values_standardized = robust_scaler.fit_transform(data.values)
+    data = pd.DataFrame(values_standardized, columns=data.columns)
+
+    # Criar modelos
+
+    clf_models = [
+        KNeighborsClassifier(),
+        SVC(),
+        GaussianProcessClassifier(),
+        DecisionTreeClassifier(),
+        RandomForestClassifier(),
+        MLPClassifier(),
+        AdaBoostClassifier(),
+        GaussianNB()
+    ]
+
+    clf_names = ["Nearest Neighbors", "SVM", "Gaussian Process", "Decision Tree", "Random Forest", "Neural Net",
+                 "AdaBoost", "Naive Bayes", "QDA"]
+
+    # K-Fold Cross-validation e Calcular resultados
+
+    for name, clf in zip(clf_names, clf_models):
+        # cv = ShuffleSplit(n_splits=10, test_size=0.6, random_state=0)
+        scores = cross_val_score(clf, data, target, cv=5)
+        print(name, "Accuracy: %0.6f (+/- %0.6f)" % (scores.mean(), scores.std() * 2))
+
 # Ler dados do ficheiro .csv
 
 data = pd.read_csv("../datasets/Dataset_MousePSS.csv", sep=';')
 
 # Remover ExamID e StudyID
 
-#data = data.drop('ExamID', 1)
+data = data.drop('ExamID', 1)
 data = data.drop('StudyID', 1)
+
+# Gerar gráficos de distribuição das features
+
+mkdir("dist")
+
+nRows = data.shape[0]
+nBins = int(round(sqrt(nRows)))
+
+for key in data.keys():
+    data.hist(column=key, bins=nBins)
+    fig_name = "dist-" + key + ".png"
+    plt.savefig("dist/" + fig_name)
 
 # Retirar coluna de output 'PSS_Stress' do conjunto de dados (para variável auxiliar)
 
@@ -44,78 +134,6 @@ data = data.drop('PSS_Stress', 1)
 # Substituir NaN por valor da mediana
 
 data = data.fillna(data.median())
-
-# Gerar gráficos de distribuição das features
-
-mkdir("figures")
-
-nRows = data.shape[0]
-nBins = int(round(sqrt(nRows)))
-
-for key in data.keys():
-    data.hist(column=key, bins=nBins)
-    fig_name = "dist-" + key + ".png"
-    plt.savefig("figures/" + fig_name)
-
-
-print("### Normalization ###\n")
-
-# Normalizar dados
-
-normalizer = preprocessing.Normalizer(norm='l1')  # ou 'l2'
-values_normalized = normalizer.transform(data.values)
-data = pd.DataFrame(values_normalized, columns=data.columns)
-
-# Feature Selection
-
-selector = SelectKBest(f_classif, k=5)
-selector.fit(data, target)
-cols = selector.get_support(indices=True)
-
-cols_names = list(data.columns[cols])
-
-for idx, (ci, cn) in enumerate(zip(cols, cols_names)):
-    print("*" * (len(cols) - idx) + " " * idx, ci, cn)
-
-data = data[cols_names]
-
-# Criar modelos
-
-clf_models = [
-    KNeighborsClassifier(),
-    SVC(),
-    GaussianProcessClassifier(),
-    DecisionTreeClassifier(),
-    RandomForestClassifier(),
-    MLPClassifier(),
-    AdaBoostClassifier(),
-    GaussianNB()
-    # QuadraticDiscriminantAnalysis()  # é o único que não está a dar
-]
-
-clf_names = ["Nearest Neighbors", "SVM", "Gaussian Process", "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-             "Naive Bayes", "QDA"]
-
-# K-Fold Cross-validation e Calcular resultados
-
-for name, clf in zip(clf_names, clf_models):
-    cv = ShuffleSplit(n_splits=10, test_size=0.6, random_state=0)
-    scores = cross_val_score(clf, data, target, cv=cv)
-    print(name, "Accuracy: %0.6f (+/- %0.6f)" % (scores.mean(), scores.std() * 2))
-
-
-print("\n\n### Standardization ###")
-
-# Standardizar dados
-
-# scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
-# values_standardized = scaler.fit_transform(data.values)
-# data = pd.DataFrame(values_standardized, columns=data.columns)
-
-# Quando temos outliers, é melhor usar outro scaler (?)
-robust_scaler = preprocessing.RobustScaler()
-values_standardized = robust_scaler.fit_transform(data.values)
-data = pd.DataFrame(values_standardized, columns=data.columns)
 
 # Feature selection
 
@@ -129,26 +147,10 @@ for idx, (ci, cn) in enumerate(zip(cols, cols_names)):
 
 data = data[cols_names]
 
-# Criar modelos
+# Comparar resultados:
 
-clf_models = [
-    KNeighborsClassifier(),
-    SVC(),
-    GaussianProcessClassifier(),
-    DecisionTreeClassifier(),
-    RandomForestClassifier(),
-    MLPClassifier(),
-    AdaBoostClassifier(),
-    GaussianNB()
-    # QuadraticDiscriminantAnalysis()  # é o único que não está a dar
-]
+## Normalizar
+normalizacao_e_resultados(data)
 
-clf_names = ["Nearest Neighbors", "SVM", "Gaussian Process", "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-             "Naive Bayes", "QDA"]
-
-# K-Fold Cross-validation e Calcular resultados
-
-for name, clf in zip(clf_names, clf_models):
-    cv = ShuffleSplit(n_splits=10, test_size=0.6, random_state=0)
-    scores = cross_val_score(clf, data, target, cv=cv)
-    print(name, "Accuracy: %0.6f (+/- %0.6f)" % (scores.mean(), scores.std() * 2))
+## Standardizar
+standardizacao_e_resultados(data)
--- tp2/optimization.py
+++ tp2/optimization.py
@@ -6,12 +6,11 @@ from sklearn import preprocessing
 from sklearn.svm import SVC
 from sklearn.model_selection import RandomizedSearchCV
 
-
 __author__ = "André Pereira, Carlos Lemos, João Barreira, Rafael Braga"
 __email__ = "pg38923@alunos.uminho.pt, pg38410@alunos.uminho.pt, a73831@alunos.uminho.pt, a61799@alunos.uminho.pt"
 
 
-# Imprime no terminal top-X (por defeito = 3) de resultados dos modelos testados
+# Imprime no terminal o top-X (por defeito = 3) de resultados da hiper-parametrização do modelo
 def report(results, n_top=3):
     for i in range(1, n_top + 1):
         candidates = np.flatnonzero(results['rank_test_score'] == i)
@@ -29,7 +28,7 @@ data = pd.read_csv("../datasets/Dataset_MousePSS.csv", sep=';')
 
 # Remover ExamID e StudyID
 
-# data = data.drop('ExamID', 1)
+data = data.drop('ExamID', 1)
 data = data.drop('StudyID', 1)
 
 # Retirar coluna de output 'PSS_Stress' do conjunto de dados (para variável auxiliar)
@@ -39,36 +38,21 @@ data = data.drop('PSS_Stress', 1)
 
 # Substituir NaN por valor da mediana
 
-for key in data.keys():
-    data[key] = data[key].fillna(data[key].median())
+data = data.fillna(data.median())
 
-# Feature selection
+# Feature selection "manual" (melhores resultados)
 
 cols = [9, 12]
 cols_names = list(data.columns[cols])
 data = data[cols_names]
 
-# selector = SelectKBest(f_regression, k=3)
-# selector.fit(data, target)
-# cols = selector.get_support(indices=True)
-# cols_names = list(data.columns[cols])
-
-for idx, (ci, cn) in enumerate(zip(cols, cols_names)):
-    print("*" * (len(cols) - idx) + " " * idx, ci, cn)
-
-# print(data.keys())
-
 # Standardizar
-
-# scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
-# values_standardized = scaler.fit_transform(data.values)
-# data = pd.DataFrame(values_standardized, columns=data.columns)
 
 robust_scaler = preprocessing.RobustScaler()
 values_standardized = robust_scaler.fit_transform(data.values)
 data = pd.DataFrame(values_standardized, columns=data.columns)
 
-# Hyper-parameterização
+# Hyper-parametrização
 
 clf_model = SVC()
 
@@ -80,19 +64,7 @@ param_dist = {'C': np.random.uniform(low=0.0, high=2.0, size=(10,)),
               'shrinking': [True, False],
               'probability': [True, False],
               'tol': np.random.uniform(low=0.0, high=2.0, size=(10,)),
-              'decision_function_shape' : ['ovo', 'ovr']}
-
-# clf_model = GaussianProcessClassifier()
-#
-# param_dist = {
-#               # 'n_restarts_optimizer': ss.randint(1, 10),
-#               # 'max_iter_predict': ss.randint(1, 10),
-#               # 'warm_start': [True, False],
-#               # 'copy_X_train': [True, False],
-#               # 'random_state': ss.randint(1, 10),
-#               'multi_class': ['one_vs_rest', 'one_vs_one'],
-#               # 'n_jobs': [-1]
-#               }
+              'decision_function_shape': ['ovo', 'ovr']}
 
 # Cálculo dos resultados (com hiper-parameterização a 50 iterações e 5-fold cross validation)
 
